94d4c55b15e8e5e752d8add31606d2846a7643b0
Aditya-Raj-75F <araj@75f.io>
2023-10-27 16:00:22 +0530

    First Commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+
+    // Plugin for UI Navigation
+    id("androidx.navigation.safeargs") version "2.7.4" apply false
+
+    // Plugin for ksp annotation processor
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+}
